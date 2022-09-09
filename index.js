cbaf43d3f767b93ff91f16fbe7f8ce36828751e2
Zeptosec <armandasmatkevicius@gmail.com>
2022-09-10 00:42:32 +0300

    Removed constant time presets

--- index.js
+++ index.js
@@ -5,20 +5,28 @@ require('dotenv').config();
 const app = express();
 
 const getSchedule = async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
     await page.goto('https://uais.cr.ktu.lt/ktuis/stud.busenos');
-    await page.click("input[type=submit]");
-    await new Promise(r => setTimeout(r, 2000));
-    await page.click("button[type=submit]")
-    await new Promise(r => setTimeout(r, 2000));
-    await page.type('#username', process.env.USER);
-    await page.type('#password', process.env.PASS);
-    await page.click('input[type=submit]');
-    await new Promise(r => setTimeout(r, 2000));
-    await page.click('#yesbutton');
+    const toLoginPage = await page.waitForSelector('input[type=submit]');
+    await toLoginPage.click();
+
+    const but1 = await page.waitForSelector('button[type=submit]');
+    await but1.click()
+
+    const userField = await page.waitForSelector('#username');
+    await userField.type(process.env.USER);
+    await page.screenshot({ path: 'user.png'});
+    const passField = await page.waitForSelector('#password');
+    await passField.type(process.env.PASS);
+    await page.screenshot({ path: 'pass.png'});
+    const loginField = await page.waitForSelector('input[type=submit]');
+    await loginField.click();
+    const yesButton = await page.waitForSelector('#yesbutton');
+    await yesButton.click();
     await new Promise(r => setTimeout(r, 10000));
+    await page.waitForSelector('#tvark_cont');
     const query = "Mano savait";
     await page.evaluate(query => {
       const elements = [...document.querySelectorAll('a')];
@@ -26,15 +34,15 @@ const getSchedule = async (req, res) => {
 
       targetElement && targetElement.click();
     }, query)
-    await new Promise(r => setTimeout(r, 5000));
     // maybe use html parser to parse instead of sending an image
-    const tabl = await page.$('#kal_div_id');
+    const tabl = await page.waitForSelector('#kal_div_id');
     await tabl.screenshot({ path: 'schedule.png' });
 
     await browser.close();
     res.status(200).sendFile(__dirname + "/schedule.png")
   } catch (err) {
     console.log(err);
+    await page.screenshot({ path: 'error.png' });
     res.status(500).json({ message: err.message });
   }
 }
